170f5d28141e1b3420a6eea684b262a747495f29
Seol <131204758+JG-0204@users.noreply.github.com>
2023-12-27 17:42:46 +0800

    Add part5b 5.7 exercise

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: '1px solid',
+    marginBottom: 5,
+  };
+
+  const blogDetailsStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 5,
+  };
+
+  return (
+    <>
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(showDetails ? false : true)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+        {showDetails && (
+          <div style={blogDetailsStyle}>
+            {blog.url}
+            <div>
+              {blog.likes} <button>like</button>
+            </div>
+            {blog.author}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Blog;
